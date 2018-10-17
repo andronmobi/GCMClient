912038d6726a4c325b7614aab565b3a3d43d0719
Andrei Mandychev <amandychev-ext@lequipe.fr>
2018-10-17 10:48:57 +0200

    Set notification channel for AndroidO

--- app/src/main/java/com/andronblog/gcmclient/MyFcmListenerService.java
+++ app/src/main/java/com/andronblog/gcmclient/MyFcmListenerService.java
@@ -16,12 +16,15 @@
 
 package com.andronblog.gcmclient;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -33,6 +36,13 @@ import java.util.Map;
 public class MyFcmListenerService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFcmListenerService";
+    private static final String NOTIFICATION_CHANNEL_ID = "test";
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d(TAG, "New token: " + s);
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -66,17 +76,24 @@ public class MyFcmListenerService extends FirebaseMessagingService {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, mId++, intent, 0);
 
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+            // Configure the notification channel.
+            notificationChannel.setDescription("FCM test channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "test")
                 .setSmallIcon(R.drawable.ic_stat_ic_notification)
                 .setContentTitle("FCM msg from " + from)
                 .setContentText(message)
                 .setAutoCancel(false)
-                .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         int id = (int) System.currentTimeMillis();
         notificationManager.notify(id, notificationBuilder.build());
