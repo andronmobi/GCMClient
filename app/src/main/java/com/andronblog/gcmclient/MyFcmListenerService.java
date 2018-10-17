ae8170b59b6bddc091fd385b98d7ba2e49af73d0
Andrei Mandychev <amandychev-ext@lequipe.fr>
2018-10-17 11:08:11 +0200

    Remove firebase-messaging

--- app/src/main/java/com/andronblog/gcmclient/MyFcmListenerService.java
+++ app/src/main/java/com/andronblog/gcmclient/MyFcmListenerService.java
@@ -16,86 +16,103 @@
 
 package com.andronblog.gcmclient;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+//import android.app.NotificationChannel;
+//import android.app.NotificationManager;
+//import android.app.PendingIntent;
+//import android.app.Service;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.graphics.Color;
+//import android.media.RingtoneManager;
+//import android.net.Uri;
+//import android.os.Build;
+//import android.os.IBinder;
+//import android.support.annotation.Nullable;
+//import android.support.v4.app.NotificationCompat;
+//import android.util.Log;
+//
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//import com.google.firebase.messaging.RemoteMessage;
+//
+//import java.util.Map;
+
+import android.app.Service;
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
+public class MyFcmListenerService extends Service {
 
-import java.util.Map;
-
-public class MyFcmListenerService extends FirebaseMessagingService {
-
-    private static final String TAG = "MyFcmListenerService";
-    private static final String NOTIFICATION_CHANNEL_ID = "test";
-
+    @Nullable
     @Override
-    public void onNewToken(String s) {
-        super.onNewToken(s);
-        Log.d(TAG, "New token: " + s);
-    }
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        String from = remoteMessage.getFrom();
-        Log.d(TAG, "From: " + from);
-
-        // Check if message contains a data payload.
-        Map data = remoteMessage.getData();
-        if (data.size() > 0) {
-            Log.d(TAG, "Message data payload: " + data);
-        }
-
-        // Check if message contains a notification payload.
-        String body = "null";
-        if (remoteMessage.getNotification() != null) {
-            body = remoteMessage.getNotification().getBody();
-            Log.d(TAG, "Message Notification Body: " + body);
-        }
-
-        sendNotification(from, body);
-    }
-
-    private int mId = 5000;
-    /**
-     * Create and show a simple notification containing the received GCM message.
-     *
-     * @param message GCM message received.
-     */
-    private void sendNotification(String from, String message) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, mId++, intent, 0);
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
-            // Configure the notification channel.
-            notificationChannel.setDescription("FCM test channel");
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.RED);
-            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
-            notificationChannel.enableVibration(true);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "test")
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("FCM msg from " + from)
-                .setContentText(message)
-                .setAutoCancel(false)
-                .setContentIntent(pendingIntent);
-
-        int id = (int) System.currentTimeMillis();
-        notificationManager.notify(id, notificationBuilder.build());
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
+
+//public class MyFcmListenerService extends FirebaseMessagingService {
+//
+//    private static final String TAG = "MyFcmListenerService";
+//    private static final String NOTIFICATION_CHANNEL_ID = "test";
+//
+//    @Override
+//    public void onNewToken(String s) {
+//        super.onNewToken(s);
+//        Log.d(TAG, "New token: " + s);
+//    }
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        String from = remoteMessage.getFrom();
+//        Log.d(TAG, "From: " + from);
+//
+//        // Check if message contains a data payload.
+//        Map data = remoteMessage.getData();
+//        if (data.size() > 0) {
+//            Log.d(TAG, "Message data payload: " + data);
+//        }
+//
+//        // Check if message contains a notification payload.
+//        String body = "null";
+//        if (remoteMessage.getNotification() != null) {
+//            body = remoteMessage.getNotification().getBody();
+//            Log.d(TAG, "Message Notification Body: " + body);
+//        }
+//
+//        sendNotification(from, body);
+//    }
+//
+//    private int mId = 5000;
+//    /**
+//     * Create and show a simple notification containing the received GCM message.
+//     *
+//     * @param message GCM message received.
+//     */
+//    private void sendNotification(String from, String message) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, mId++, intent, 0);
+//
+//        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+//            // Configure the notification channel.
+//            notificationChannel.setDescription("FCM test channel");
+//            notificationChannel.enableLights(true);
+//            notificationChannel.setLightColor(Color.RED);
+//            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+//            notificationChannel.enableVibration(true);
+//            notificationManager.createNotificationChannel(notificationChannel);
+//        }
+//
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "test")
+//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+//                .setContentTitle("FCM msg from " + from)
+//                .setContentText(message)
+//                .setAutoCancel(false)
+//                .setContentIntent(pendingIntent);
+//
+//        int id = (int) System.currentTimeMillis();
+//        notificationManager.notify(id, notificationBuilder.build());
+//    }
+//}
